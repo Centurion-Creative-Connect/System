9ce9a04257b07074db1ddaf449bc606c9c5239e8
DerpyNewbie <derpynewbie@gmail.com>
2022-10-29 06:59:21 +0900

    Remove hitLocal/hitRemote subcommand
    Due to change of local hit logic structure, GameManager should not
    have reference to LocalHitEffect, thus holding this subcommand within
    GameManagerCommand should not be optimum, though I'm lazy soo it's
    staying right here.

--- Packages/org.centurioncc.system.commands/Runtime/Command/GameManagerCommand.cs
+++ Packages/org.centurioncc.system.commands/Runtime/Command/GameManagerCommand.cs
@@ -13,7 +13,7 @@ namespace CenturionCC.System.Command
         public override string Label => "GameManager";
         public override string[] Aliases => new[] { "Game" };
         public override string Usage =>
-            "<command> <canShoot|hitLocal|hitRemote|isMod|useHaptic|version|license>";
+            "<command> <canShoot|testHit|isMod|useHaptic|version|license>";
 
         private void Start()
         {
@@ -31,19 +31,12 @@ namespace CenturionCC.System.Command
                     console.Println($"CanShoot: {_gameManager.CanShoot()}");
                     return ConsoleLiteral.Of(_gameManager.CanShoot());
                 }
-                case "hitlocal":
-                case "testhitlocal":
+                case "testhit":
                 {
-                    console.Println("Playing local hit as dummy");
-                    _gameManager.PlayHitLocal(null);
-                    return ConsoleLiteral.GetNone();
-                }
-                case "hitremote":
-                case "testhitremote":
-                {
-                    console.Println("Playing remote hit");
+                    console.Println("Playing onDeath for all players");
                     foreach (var player in _gameManager.players.GetPlayers())
-                        _gameManager.PlayHitRemote(player);
+                        if (player != null)
+                            player.OnDeath();
                     return ConsoleLiteral.GetNone();
                 }
                 case "mod":
