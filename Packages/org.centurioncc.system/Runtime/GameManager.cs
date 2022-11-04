51cee54bd505fd3d4e6acbe54a537103d29a28b3
DerpyNewbie <derpynewbie@gmail.com>
2022-11-04 19:57:57 +0900

    Bump version up to `0.2.1`

--- Packages/org.centurioncc.system/Runtime/GameManager.cs
+++ Packages/org.centurioncc.system/Runtime/GameManager.cs
@@ -63,7 +63,7 @@ namespace CenturionCC.System
 
         public static string GetVersion()
         {
-            return "0.2.0";
+            return "0.2.1";
         }
 
         public int KeepAlive(WatchdogProc wd, int nonce)
