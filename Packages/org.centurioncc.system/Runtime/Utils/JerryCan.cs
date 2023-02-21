f130695cc0b6cd8a4d7c3beab34423da91aa6154
DerpyNewbie <derpynewbie@gmail.com>
2023-02-22 06:43:47 +0900

    Add more object gimmick scripts

---
+++ Packages/org.centurioncc.system/Runtime/Utils/JerryCan.cs
@@ -0,0 +1,50 @@
+﻿using CenturionCC.System.Player;
+using CenturionCC.System.UI;
+using DerpyNewbie.Common;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+
+namespace CenturionCC.System.Utils
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class JerryCan : PlayerManagerCallbackBase
+    {
+        private const float PickupCooldownTime = 10F;
+        [SerializeField]
+        private VRCPickup pickup;
+        [SerializeField]
+        private TranslatableMessage dropMessage;
+
+        private NotificationUI _notificationUI;
+
+        private void Start()
+        {
+            if (pickup == null)
+                pickup = (VRCPickup)GetComponent(typeof(VRCPickup));
+            if (_notificationUI == null)
+                _notificationUI = CenturionSystemReference.GetNotificationUI();
+
+            CenturionSystemReference.GetPlayerManager().SubscribeCallback(this);
+        }
+
+        public override void OnKilled(PlayerBase firedPlayer, PlayerBase hitPlayer)
+        {
+            if (pickup.currentPlayer == hitPlayer.VrcPlayer && hitPlayer.IsLocal)
+                Drop();
+        }
+
+        private void Drop()
+        {
+            pickup.Drop();
+            if (_notificationUI != null && dropMessage != null)
+                _notificationUI.ShowWarn(dropMessage.Message);
+            SendCustomEventDelayedSeconds(nameof(MakePickupable), PickupCooldownTime);
+        }
+
+        public void MakePickupable()
+        {
+            pickup.pickupable = true;
+        }
+    }
+}
---
+++ Packages/org.centurioncc.system/Runtime/Utils/Shield.cs
@@ -0,0 +1,55 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+
+namespace CenturionCC.System.Utils
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class Shield : UdonSharpBehaviour
+    {
+        [SerializeField]
+        private VRCPickup pickup;
+        [SerializeField]
+        private Transform pickupReference;
+        [SerializeField]
+        private Transform leftHandedReference;
+        [SerializeField]
+        private Transform rightHandedReference;
+
+        private bool isHeldLocally;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (isHeldLocally)
+                Networking.LocalPlayer.PlayHapticEventInHand(pickup.currentHand, 0.1F, 2F, 1F);
+        }
+
+        private void OnCollisionStay(Collision collisionInfo)
+        {
+            if (isHeldLocally)
+                Networking.LocalPlayer.PlayHapticEventInHand(pickup.currentHand, .2F, .02F, .1F);
+        }
+
+        public override void OnPickup()
+        {
+            var refTransform = pickup.currentHand == VRC_Pickup.PickupHand.Left
+                ? leftHandedReference
+                : rightHandedReference;
+
+            pickupReference.localPosition = refTransform.localPosition;
+            pickupReference.localRotation = refTransform.localRotation;
+
+            isHeldLocally = true;
+            pickup.pickupable = false;
+
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        public override void OnDrop()
+        {
+            isHeldLocally = false;
+            pickup.pickupable = true;
+        }
+    }
+}
