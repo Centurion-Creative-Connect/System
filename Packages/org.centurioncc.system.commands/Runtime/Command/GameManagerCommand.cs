68e819ca7713218b61b99fef7a55512c204cb4a4
DerpyNewbie <derpynewbie@gmail.com>
2023-05-09 06:40:17 +0900

    Bump version up to 0.4.1

--- Packages/org.centurioncc.system.commands/Runtime/Command/GameManagerCommand.cs
+++ Packages/org.centurioncc.system.commands/Runtime/Command/GameManagerCommand.cs
@@ -94,7 +94,7 @@ namespace CenturionCC.System.Command
                 case "version":
                 {
                     console.Println($"Centurion System   - v{GameManager.GetVersion()}");
-                    console.Println("Centurion System Commands - v0.4.0");
+                    console.Println("Centurion System Commands - v0.4.1");
                     return GameManager.GetVersion();
                 }
                 case "license":
