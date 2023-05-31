abe96798b80cfef1562b203e721ed431ea02fb9a
DerpyNewbie <derpynewbie@gmail.com>
2023-05-31 13:32:20 +0900

    Move shared logics to ExternalPlayerTagBase

--- Packages/org.centurioncc.system/Runtime/Player/External/PlayerTag/ExternalPlayerTag.cs
+++ Packages/org.centurioncc.system/Runtime/Player/External/PlayerTag/ExternalPlayerTag.cs
@@ -2,7 +2,6 @@
 using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
-using VRC.SDKBase;
 
 namespace CenturionCC.System.Player.External.PlayerTag
 {
@@ -24,71 +23,29 @@ namespace CenturionCC.System.Player.External.PlayerTag
         private Image creatorTag;
         [SerializeField]
         private Image masterTag;
-        private readonly Vector3 _nametagOffsetPosition = Vector3.up * 0.3F;
-        private bool _didSetup;
-        private bool _didStart;
-        private VRCPlayerApi _localPlayer;
 
-        private ExternalPlayerTagManager _tagManager;
-        private Transform _transform;
-
-        private void Start()
+        protected override void Start()
         {
-            Debug.Log($"[{name}] Start");
-            _localPlayer = Networking.LocalPlayer;
-            _didStart = true;
-            if (_didSetup)
+            base.Start();
+            if (didSetup)
                 updateManager.SubscribeUpdate(this);
         }
 
         private void OnEnable()
         {
-            Debug.Log($"[{name}] OnEnable: {updateManager == null}");
-            if (_didStart)
+            if (didStart)
                 updateManager.SubscribeUpdate(this);
         }
 
         private void OnDisable()
         {
-            Debug.Log($"[{name}] OnDisable: {updateManager == null}");
             updateManager.UnsubscribeUpdate(this);
         }
 
-        private void OnDestroy()
+        protected override void OnDestroy()
         {
-            Debug.Log($"[{name}] OnDestroy");
-
+            base.OnDestroy();
             updateManager.UnsubscribeUpdate(this);
-            _tagManager.RemovePlayerTag(this);
-        }
-
-        public void _Update()
-        {
-            if (!Utilities.IsValid(followingPlayer))
-            {
-                DestroyThis();
-                return;
-            }
-
-            _transform.position = followingPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position +
-                                  _nametagOffsetPosition;
-            _transform.LookAt(_localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head).position);
-        }
-
-        public override void OnPlayerLeft(VRCPlayerApi player)
-        {
-            if (followingPlayer == player)
-                DestroyThis();
-        }
-
-        public override void Setup(ExternalPlayerTagManager manager, VRCPlayerApi api)
-        {
-            gameObject.SetActive(true);
-            _transform = transform;
-            _tagManager = manager;
-            followingPlayer = api;
-            _localPlayer = Networking.LocalPlayer;
-            _didSetup = true;
         }
 
         public override void SetTagOn(TagType type, bool isOn)
@@ -130,7 +87,7 @@ namespace CenturionCC.System.Player.External.PlayerTag
             teamTag.color = teamColor;
         }
 
-        public bool IsVisible()
+        private bool IsVisible()
         {
             return teamTag.gameObject.activeSelf || masterTag.gameObject.activeSelf ||
                    creatorTag.gameObject.activeSelf || staffTag.gameObject.activeSelf ||
