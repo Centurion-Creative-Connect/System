0a9ed90b00ebad675469d797cce7ad78d766511e
DerpyNewbie <derpynewbie@gmail.com>
2023-09-01 01:33:33 +0900

    temp commit

---
+++ Packages/org.centurioncc.system/Runtime/Utils/FeatureFlags.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UdonSharp;
+
+namespace CenturionCC.System.Utils
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class FeatureFlags : UdonSharpBehaviour
+    {
+        // These field will be changed using UdonBehaviour#SetProgramVariable
+        // ReSharper disable FieldCanBeMadeReadOnly.Global ConvertToConstant.Global
+        [NonSerialized]
+        public bool useVictimRequest = false;
+        [NonSerialized]
+        public bool useConditionalResultCheck = false;
+        [NonSerialized]
+        public bool doNotifyIfHitFailed = true;
+        [NonSerialized]
+        public bool makeSyncerPlayerIdDependent = true;
+        // ReSharper restore FieldCanBeMadeReadOnly.Global ConvertToConstant.Global
+    }
+}
