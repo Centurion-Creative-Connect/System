11f8870c01e620e7c696da8a0b3d9abcde7b163a
DerpyNewbie <derpynewbie@gmail.com>
2023-09-01 01:33:33 +0900

    temp commit

---
+++ Packages/org.centurioncc.system/Tests/DamageDataResolverSyncTest.cs
@@ -0,0 +1,10 @@
+﻿using CenturionCC.System.Player;
+
+namespace CenturionCC.System.Tests
+{
+    public class DamageDataResolverSyncTest
+    {
+        private DamageDataResolver _resolver1;
+        private DamageDataResolver _resolver2;
+    }
+}
