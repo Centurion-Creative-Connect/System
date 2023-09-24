13859bdfd1f6921f2f3e96d8754fe6f6fae6f3b8
DerpyNewbie <derpynewbie@gmail.com>
2023-09-24 21:26:29 +0900

    0.5.3-rc.3

--- Packages/org.centurioncc.system/Runtime/GameManager.cs
+++ Packages/org.centurioncc.system/Runtime/GameManager.cs
@@ -63,7 +63,7 @@ namespace CenturionCC.System
 
         public static string GetVersion()
         {
-            return "0.5.3-rc.2";
+            return "0.5.3-rc.3";
         }
 
         public int KeepAlive(WatchdogProc wd, int nonce)
