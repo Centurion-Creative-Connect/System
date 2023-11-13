8e455d514a1bdc8d0679c34e296f39645927cd97
DerpyNewbie <derpynewbie@gmail.com>
2023-11-13 17:52:21 +0900

    Fix broken checks and cleaned up unused properties

--- Packages/org.centurioncc.system/Tests/DamageDataResolverInvincibleTest.cs
+++ Packages/org.centurioncc.system/Tests/DamageDataResolverInvincibleTest.cs
@@ -1,69 +1,65 @@
 ﻿using System;
 using CenturionCC.System.Player;
 using NUnit.Framework;
-using UnityEngine;
 
 namespace CenturionCC.System.Tests
 {
     public class DamageDataResolverInvincibleTest
     {
-        private DamageDataResolver _instance;
-
-        [SetUp]
-        public void Setup()
-        {
-            _instance = new GameObject().AddComponent<DamageDataResolver>();
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            UnityEngine.Object.DestroyImmediate(_instance.gameObject);
-            _instance = null;
-        }
-
-        [Test]
-        public void TestInvincibleTime()
-        {
-            _instance.InvincibleDuration = 10D;
-            var now = new DateTime(2022, 3, 25, 1, 0, 0);
-            Assert.That(_instance.IsInInvincibleDuration(now, now));
-            Assert.That(_instance.IsInInvincibleDuration(now, now.AddSeconds(-1D)));
-            Assert.That(_instance.IsInInvincibleDuration(now, now.AddSeconds(-9D)));
-            Assert.That(_instance.IsInInvincibleDuration(now, now.AddSeconds(-10D)) == false);
-            Assert.That(_instance.IsInInvincibleDuration(now, now.AddSeconds(-11D)) == false);
-            Assert.That(_instance.IsInInvincibleDuration(now, now.AddSeconds(1D)) == false);
-        }
-
         [Test]
         public void TestComputeHitResultFromDateTime()
         {
-            _instance.InvincibleDuration = 10D;
             var now = new DateTime(2022, 3, 25, 1, 0, 0);
-            var outOfDuration = now.AddSeconds(-50D);
+            var after = now.AddSeconds(5);
+            var before = now.AddSeconds(-5D);
 
-            void Check(DateTime origin, DateTime hit, DateTime atkDied, DateTime vicDied, bool expected)
+            // Origin before
+            Check(before, before, before, true);
+            Check(before, now, before, true);
+            Check(before, after, before, true);
+
+            Check(before, before, now, true);
+            Check(before, now, now, true);
+            Check(before, after, now, false);
+
+            Check(before, before, after, true);
+            Check(before, now, after, true);
+            Check(before, after, after, true);
+
+
+            // Origin now
+            Check(now, before, before, true);
+            Check(now, now, before, true);
+            Check(now, after, before, true);
+
+            Check(now, before, now, true);
+            Check(now, now, now, true);
+            Check(now, after, now, true);
+
+            Check(now, before, after, false);
+            Check(now, now, after, true);
+            Check(now, after, after, true);
+
+
+            // Origin after
+            Check(after, before, before, true);
+            Check(after, now, before, false);
+            Check(after, after, before, true);
+
+            Check(after, before, now, true);
+            Check(after, now, now, true);
+            Check(after, after, now, true);
+
+            Check(after, before, after, true);
+            Check(after, now, after, true);
+            Check(after, after, after, true);
+            return;
+
+            void Check(DateTime origin, DateTime atkDied, DateTime atkRev, bool expected)
             {
-                var result = _instance.ComputeHitResultFromDateTime(origin, hit, atkDied, vicDied);
-                Assert.That(result == expected, "Expected: {0}\nBut was: {1}", expected, result);
+                var result = DamageDataResolver.ComputeHitResultFromDateTime(origin, atkDied, atkRev);
+                Assert.That(result, Is.EqualTo(expected));
             }
-
-            Check(now.AddSeconds(10D), now.AddSeconds(10D), now, outOfDuration, true);
-            Check(now.AddSeconds(10D), now.AddSeconds(11D), now, outOfDuration, true);
-
-            Check(now.AddSeconds(10D), now.AddSeconds(10D), outOfDuration, now, true);
-            Check(now.AddSeconds(10D), now.AddSeconds(11D), outOfDuration, now, true);
-
-            Check(now.AddSeconds(1D), now, now, now, false);
-            Check(now.AddSeconds(11D), now.AddSeconds(10D), now, outOfDuration, false);
-            Check(now.AddSeconds(11D), now.AddSeconds(10D), outOfDuration, now, false);
-
-            Check(now, now, now, now, false);
-            Check(now.AddSeconds(1D), now.AddSeconds(2D), now, outOfDuration, false);
-            Check(now.AddSeconds(9D), now.AddSeconds(10D), now, outOfDuration, false);
-
-            Check(now, now.AddSeconds(1D), outOfDuration, now, false);
-            Check(now, now.AddSeconds(9D), outOfDuration, now, false);
         }
     }
 }
