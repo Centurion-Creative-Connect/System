a9103103613332ead8891153b15add39b5d29b33
DerpyNewbie <derpynewbie@gmail.com>
2023-11-18 20:42:38 +0900

    Bump version to `0.6.0-prealpha+centurion.1`

--- Packages/org.centurioncc.system/Runtime/GameManager.cs
+++ Packages/org.centurioncc.system/Runtime/GameManager.cs
@@ -60,7 +60,7 @@ namespace CenturionCC.System
 
         public static string GetVersion()
         {
-            return "0.5.3";
+            return "0.6.0-prealpha+centurion.1";
         }
 
         public int KeepAlive(WatchdogProc wd, int nonce)
