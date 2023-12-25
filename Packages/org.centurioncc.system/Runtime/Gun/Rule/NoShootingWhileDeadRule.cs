d867e8f893894f498c943db8d1e99ac5d1eb4d03
DerpyNewbie <derpynewbie@gmail.com>
2023-12-25 16:56:16 +0900

    Add new ShootingRule abstraction to handle custom notification messages

--- Packages/org.centurioncc.system/Runtime/Gun/Rule/NoShootingWhileDeadRule.cs
+++ Packages/org.centurioncc.system/Runtime/Gun/Rule/NoShootingWhileDeadRule.cs
@@ -6,19 +6,24 @@ using UnityEngine;
 namespace CenturionCC.System.Gun.Rule
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-    public class NoShootingWhileDeadRule : GunManagerCallbackBase
+    public class NoShootingWhileDeadRule : ShootingRule
     {
         [SerializeField] [HideInInspector] [NewbieInject]
         private GunManager gunManager;
         [SerializeField] [HideInInspector] [NewbieInject]
         private PlayerManager playerManager;
+        [SerializeField]
+        private TranslatableMessage cancelledMessage;
+
+        public override int RuleId => 1001;
+        public override TranslatableMessage CancelledMessage => cancelledMessage;
 
         private void Start()
         {
-            gunManager.SubscribeCallback(this);
+            gunManager.AddShootingRule(this);
         }
 
-        public override bool CanShoot()
+        public override bool CanLocalShoot(GunBase instance)
         {
             var localPlayer = playerManager.GetLocalPlayer();
             return localPlayer == null || !localPlayer.IsDead;
--- Packages/org.centurioncc.system/Runtime/Gun/Rule/NoShootingWhileNotInGameRule.cs
+++ Packages/org.centurioncc.system/Runtime/Gun/Rule/NoShootingWhileNotInGameRule.cs
@@ -6,19 +6,24 @@ using UnityEngine;
 namespace CenturionCC.System.Gun.Rule
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
-    public class NoShootingWhileNotInGameRule : GunManagerCallbackBase
+    public class NoShootingWhileNotInGameRule : ShootingRule
     {
         [SerializeField] [HideInInspector] [NewbieInject]
         private GunManager gunManager;
         [SerializeField] [HideInInspector] [NewbieInject]
         private PlayerManager playerManager;
+        [SerializeField]
+        private TranslatableMessage cancelledMessage;
+
+        public override int RuleId => 1002;
+        public override TranslatableMessage CancelledMessage => cancelledMessage;
 
         private void Start()
         {
-            gunManager.SubscribeCallback(this);
+            gunManager.AddShootingRule(this);
         }
 
-        public override bool CanShoot()
+        public override bool CanLocalShoot(GunBase instance)
         {
             return playerManager.HasLocalPlayer();
         }
