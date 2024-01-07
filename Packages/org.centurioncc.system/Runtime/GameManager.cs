52b45717bf8d71cf6dcd79081e7194bb304160fc
DerpyNewbie <derpynewbie@gmail.com>
2024-01-07 19:39:54 +0900

    0.6.0-rc.1

--- Packages/org.centurioncc.system/Runtime/GameManager.cs
+++ Packages/org.centurioncc.system/Runtime/GameManager.cs
@@ -56,7 +56,7 @@ namespace CenturionCC.System
 
         public static string GetVersion()
         {
-            return "0.6.0-beta.2";
+            return "0.6.0-rc.1";
         }
 
         public int KeepAlive(WatchdogProc wd, int nonce)
