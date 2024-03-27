4843aa066a1ba1b0c653df6196a1c925da5251ee
DerpyNewbie <derpynewbie@gmail.com>
2024-03-27 17:04:36 +0900

    Revert "Fix LookAtTargetOffset does not consider X/Z SubHandle offset"

--- Packages/org.centurioncc.system/Runtime/Gun/Gun.cs
+++ Packages/org.centurioncc.system/Runtime/Gun/Gun.cs
@@ -564,15 +564,9 @@ namespace CenturionCC.System.Gun
         [PublicAPI]
         public virtual float SubHandleRePickupDelay => subHandleRePickupDelay;
         [PublicAPI]
-        public virtual Vector3 LookAtTargetOffset
-        {
-            get
-            {
-                var offset = MainHandlePositionOffset - SubHandlePositionOffset;
-                // I have no idea why multiplying z by -2 fixes x/z offset issue. but it fixes the problem, so who cares!
-                return new Vector3(offset.x, offset.y, offset.z * -2);
-            }
-        }
+        public virtual Vector3 LookAtTargetOffset =>
+            new Vector3(0, MainHandlePositionOffset.y - SubHandlePositionOffset.y, 0);
+
 
         /// <summary>
         /// Local position of where bullets shooting out from.
