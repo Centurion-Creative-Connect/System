1a20a1c5e2aead55b02c0f7351c6089e805e2100
DerpyNewbie <derpynewbie@gmail.com>
2024-03-29 12:59:34 +0900

    Add inspector for GunRecoilPatternDataStore

---
+++ Packages/org.centurioncc.system/Editor/EditorInspector/Gun/DataStore/GunRecoilPatternDataStoreEditor.cs
@@ -0,0 +1,47 @@
+﻿using CenturionCC.System.Editor.Utils;
+using CenturionCC.System.Gun.DataStore;
+using UdonSharpEditor;
+using UnityEditor;
+using UnityEngine;
+
+namespace CenturionCC.System.Editor.EditorInspector.Gun.DataStore
+{
+    [CustomEditor(typeof(GunRecoilPatternDataStore))]
+    public class GunRecoilPatternDataStoreEditor : UnityEditor.Editor
+    {
+        private int _maxPatterns;
+
+        private void OnEnable()
+        {
+            _maxPatterns = ((GunRecoilPatternDataStore)target).MaxPatterns;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target))
+                return;
+
+            GUILayout.Label("Settings", EditorStyles.boldLabel);
+
+            var so = serializedObject;
+            var property = so.GetIterator();
+            property.NextVisible(true);
+            while (property.NextVisible(false))
+            {
+                GUIUtil.FoldoutPropertyField(property, 2);
+            }
+
+            if (so.ApplyModifiedProperties())
+            {
+                _maxPatterns = ((GunRecoilPatternDataStore)target).MaxPatterns;
+            }
+
+            GUILayout.Label("Stats", EditorStyles.boldLabel);
+            using (new EditorGUI.DisabledGroupScope(true))
+            {
+                EditorGUILayout.IntField("Max Patterns", _maxPatterns);
+                EditorGUILayout.HelpBox("Max Patterns does not consider duplicated elements", MessageType.Warning);
+            }
+        }
+    }
+}
