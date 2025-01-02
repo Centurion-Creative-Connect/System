79dba6a94f886b1ab5d252d3451d09389071e115
DerpyNewbie <contact@derpynewbie.dev>
2025-01-03 04:25:21 +0900

    0.6.0-rc.11

--- Packages/org.centurioncc.system/Runtime/GameManager.cs
+++ Packages/org.centurioncc.system/Runtime/GameManager.cs
@@ -59,7 +59,7 @@ namespace CenturionCC.System
 
         public static string GetVersion()
         {
-            return "0.6.0-rc.10";
+            return "0.6.0-rc.11";
         }
 
         public int KeepAlive(WatchdogProc wd, int nonce)
