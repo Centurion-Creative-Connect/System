858e6c3c693d3253c026889bcbe65fd3e90f8a8c
DerpyNewbie <contact@derpynewbie.dev>
2025-04-24 09:49:16 +0900

    Add priority setting for AudioDataStore and AudioManager

--- Packages/org.centurioncc.system/Runtime/Audio/AudioDataStore.cs
+++ Packages/org.centurioncc.system/Runtime/Audio/AudioDataStore.cs
@@ -8,25 +8,36 @@ namespace CenturionCC.System.Audio
     {
         [SerializeField]
         private AudioClip[] clips;
-        [Header("Volume")]
-        [SerializeField, Range(0F, 1F)]
+
+        [Header("Volume"), SerializeField, Range(0F, 1F)]
         private float maxVolume = 1;
+
         [SerializeField, Range(0F, 1F)]
         private float minVolume = 1;
+
         [Header("Pitch")]
         [SerializeField, Range(0F, 2F)]
         private float maxPitch = 1;
+
         [SerializeField, Range(0F, 2F)]
         private float minPitch = 1;
+
         [Header("3D Settings")]
         [SerializeField, Range(0F, 5F)]
         private float dopplerLevel = 1;
+
         [SerializeField, Range(0F, 360F)]
         private float spread = 0F;
+
         [SerializeField]
         private float minDistance = 0.5F;
+
         [SerializeField]
         private float maxDistance = 25F;
+
+        [SerializeField]
+        private int priority = 0;
+
         private int _cachedClipsLength;
         private bool _cachedIsClipsNull;
 
@@ -46,11 +57,13 @@ namespace CenturionCC.System.Audio
                 return _cachedIsClipsNull ? null : clips[Random.Range(0, _cachedClipsLength)];
             }
         }
+
         public float Volume => Random.Range(minVolume, maxVolume);
         public float Pitch => Random.Range(minPitch, maxPitch);
         public float DopplerLevel => dopplerLevel;
         public float Spread => spread;
         public float MinDistance => minDistance;
         public float MaxDistance => maxDistance;
+        public int Priority => priority;
     }
 }
