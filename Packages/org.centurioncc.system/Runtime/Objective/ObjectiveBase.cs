9667fc3ed24fc028c41b679ac4f3a9dcc19b900d
DerpyNewbie <contact@derpynewbie.dev>
2025-05-02 01:10:11 +0900

    Add ObjectiveBase and ObjectManager

---
+++ Packages/org.centurioncc.system/Runtime/Objective/ObjectiveBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using DerpyNewbie.Common;
+using UdonSharp;
+using UnityEngine;
+
+namespace CenturionCC.System.Objective
+{
+    public abstract class ObjectiveBase : UdonSharpBehaviour
+    {
+        [SerializeField] [NewbieInject]
+        protected ObjectiveManager objectiveManager;
+
+        /// <summary>
+        /// Objective's current team id.
+        /// </summary>
+        /// <remarks>
+        /// Most likely to be set in OnObjectiveStart.
+        /// Might change dynamically depending on objective.
+        /// Should never change when HasCompleted is true.
+        /// </remarks>
+        public abstract int OwningTeamId { get; }
+
+        /// <summary>
+        /// Has the objective been completed? 
+        /// </summary>
+        /// <remarks>
+        /// OwningTeamId should never change when this returns true.
+        /// </remarks>
+        public abstract bool HasCompleted { get; }
+
+        /// <summary>
+        /// Called when objective should initialize.
+        /// </summary>
+        /// <param name="teamId"></param>
+        public abstract void OnObjectiveSetup(int teamId);
+
+        /// <summary>
+        /// Called when objectyive should activate and start.
+        /// </summary>
+        public abstract void OnObjectiveStart();
+
+        /// <summary>
+        /// Called when objective should pause and halt updates.
+        /// </summary>
+        public abstract void OnObjectivePause();
+
+        /// <summary>
+        /// Called when objective should end.
+        /// </summary>
+        public abstract void OnObjectiveEnd();
+    }
+}
---
+++ Packages/org.centurioncc.system/Runtime/Objective/ObjectiveManager.cs
@@ -0,0 +1,74 @@
+﻿using JetBrains.Annotations;
+using UdonSharp;
+using VRC.SDK3.Data;
+
+namespace CenturionCC.System.Objective
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class ObjectiveManager : UdonSharpBehaviour
+    {
+        private readonly DataDictionary _teamObjectivesDict = new DataDictionary();
+
+        /// <summary>
+        /// Adds objective for specified team.
+        /// </summary>
+        /// <param name="objective"></param>
+        /// <param name="teamId"></param>
+        [PublicAPI]
+        public void AddObjective(ObjectiveBase objective, int teamId)
+        {
+            GetTeamObjectives(teamId).Add(objective);
+        }
+
+        /// <summary>
+        /// Start all objectives.
+        /// </summary>
+        [PublicAPI]
+        public void StartObjectives()
+        {
+            var keys = _teamObjectivesDict.GetKeys().ToArray();
+            foreach (var key in keys)
+            {
+                var objectives = _teamObjectivesDict[key].DataList.ToArray();
+                foreach (var objective in objectives)
+                {
+                    ((ObjectiveBase)objective.Reference).OnObjectiveStart();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Resets objective then clears current objectives list.
+        /// </summary>
+        [PublicAPI]
+        public void EndObjectives()
+        {
+            var keys = _teamObjectivesDict.GetKeys().ToArray();
+            foreach (var key in keys)
+            {
+                var objectives = _teamObjectivesDict[key].DataList.ToArray();
+                foreach (var objective in objectives)
+                {
+                    ((ObjectiveBase)objective.Reference).OnObjectiveEnd();
+                }
+            }
+
+            _teamObjectivesDict.Clear();
+        }
+
+        /// <summary>
+        /// Retrieves current active objectives for specified team.
+        /// </summary>
+        /// <param name="teamId"></param>
+        /// <returns></returns>
+        [PublicAPI]
+        public DataList GetTeamObjectives(int teamId)
+        {
+            if (_teamObjectivesDict.TryGetValue(teamId, TokenType.DataList, out var teamObjectives))
+                return teamObjectives.DataList;
+            teamObjectives = new DataToken(new DataList());
+            _teamObjectivesDict.SetValue(teamId, teamObjectives);
+            return teamObjectives.DataList;
+        }
+    }
+}
