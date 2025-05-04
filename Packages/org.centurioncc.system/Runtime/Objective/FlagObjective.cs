90af6b5b0d5b23179caaba9feef0865f66edbaf4
DerpyNewbie <contact@derpynewbie.dev>
2025-05-04 23:44:46 +0900

    WIP: Add FlagObjective

---
+++ Packages/org.centurioncc.system/Runtime/Objective/FlagObjective.cs
@@ -0,0 +1,24 @@
+﻿using UdonSharp;
+using NotImplementedException = System.NotImplementedException;
+
+namespace CenturionCC.System.Objective
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class FlagObjective : ObjectiveBase
+    {
+        public override void OnObjectiveStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnObjectivePause()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnObjectiveEnd()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
