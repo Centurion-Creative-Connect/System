87cf5d8316a27f8d02b1f02caaca11d2fd52bee9
DerpyNewbie <contact@derpynewbie.dev>
2025-06-04 13:16:31 +0900

    Merge `TagType` into `PlayerEnums` for better file management

--- Packages/org.centurioncc.system/Runtime/Player/PlayerEnums.cs
+++ Packages/org.centurioncc.system/Runtime/Player/PlayerEnums.cs
@@ -26,6 +26,18 @@
         Never,
     }
 
+    public enum TagType
+    {
+        Debug,
+        Team,
+        Master,
+        Staff,
+        Dev,
+        Owner,
+        Creator,
+        Hit
+    }
+
     public static class PlayerEnums
     {
         public static string ToEnumName(this KillType type)
@@ -82,6 +94,33 @@
 
             return "UNDEFINED_RANGE";
         }
+
+
+        public static string ToEnumName(this TagType type)
+        {
+            switch (type)
+            {
+                case TagType.Debug:
+                    return "Debug";
+                case TagType.Team:
+                    return "Team";
+                case TagType.Master:
+                    return "Master";
+                case TagType.Staff:
+                    return "Staff";
+                case TagType.Dev:
+                    return "Dev";
+                case TagType.Owner:
+                    return "Owner";
+                case TagType.Creator:
+                    return "Creator";
+                case TagType.Hit:
+                    return "Hit";
+            }
+
+            return "UNDEFINED_RANGE";
+        }
+
 
         public static byte ToByte(this BodyParts parts)
         {
