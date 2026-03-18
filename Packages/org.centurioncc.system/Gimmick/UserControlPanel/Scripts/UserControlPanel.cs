9356182e1d06fc024d79577acd31ec80cf6668b1
DerpyNewbie <contact@derpynewbie.dev>
2026-03-18 22:41:07 +0900

    Make pitch offset configurable in VR with config panel
    Closes #311

--- Packages/org.centurioncc.system/Gimmick/UserControlPanel/Scripts/UserControlPanel.cs
+++ Packages/org.centurioncc.system/Gimmick/UserControlPanel/Scripts/UserControlPanel.cs
@@ -1,7 +1,9 @@
 ﻿using CenturionCC.System.Gun;
 using DerpyNewbie.Common;
+using System;
 using UdonSharp;
 using UnityEngine;
+using UnityEngine.UI;
 namespace CenturionCC.System.Gimmick.UserControlPanel
 {
     [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
@@ -10,9 +12,21 @@ namespace CenturionCC.System.Gimmick.UserControlPanel
         [SerializeField] [NewbieInject]
         private GunController gunController;
         [SerializeField]
+        private Slider zRotationOffsetSlider;
+        [SerializeField]
         private VRActionTypeDropdown fireModeChangeDropdown;
         [SerializeField]
         private VRActionTypeDropdown reloadChangeDropdown;
+
+        [Header("Default")]
+        [SerializeField]
+        private VRActionType defaultReloadAction = VRActionType.InputJump | VRActionType.GunDirectionDown;
+        [SerializeField]
+        private VRActionType defaultFireModeAction = VRActionType.InputJump;
+        [SerializeField]
+        private float defaultZRotationOffset = 0;
+
+        private bool _isUpdatingDisplay;
 
         private void Start()
         {
@@ -22,10 +36,26 @@ namespace CenturionCC.System.Gimmick.UserControlPanel
             UpdateDisplay();
         }
 
+        private void OnMouseEnter()
+        {
+            UpdateDisplay();
+        }
+
         public void UpdateDisplay()
         {
+            if (_isUpdatingDisplay) return;
+            _isUpdatingDisplay = true;
+
+            if (zRotationOffsetSlider) zRotationOffsetSlider.value = gunController.CurrentPrimaryXOffset;
             if (fireModeChangeDropdown) fireModeChangeDropdown.SetValueWithoutNotify((int)gunController.FireModeAction);
             if (reloadChangeDropdown) reloadChangeDropdown.SetValueWithoutNotify((int)gunController.ReloadAction);
+
+            _isUpdatingDisplay = false;
+        }
+
+        public void OnPrimaryXOffsetChange()
+        {
+            gunController.CurrentPrimaryXOffset = zRotationOffsetSlider.value;
         }
 
         public void OnFireModeChange()
@@ -36,6 +66,15 @@ namespace CenturionCC.System.Gimmick.UserControlPanel
         public void OnReloadChange()
         {
             gunController.ReloadAction = (VRActionType)reloadChangeDropdown.GetValue();
+        }
+
+        public void ResetToDefault()
+        {
+            gunController.CurrentPrimaryXOffset = defaultZRotationOffset;
+            gunController.ReloadAction = defaultReloadAction;
+            gunController.FireModeAction = defaultFireModeAction;
+
+            UpdateDisplay();
         }
     }
 }
